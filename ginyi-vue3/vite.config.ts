f46374bc9627afba5b2c00dcc214f121ce3d0e82
Ginyi <ginyi@aliyun.com>
2023-01-04 22:03:50 +0800

    解决启动项目时use --host to expose✔

--- ginyi-vue3/vite.config.ts
+++ ginyi-vue3/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    server: {
+        host: '0.0.0.0',
+        open: true,
+        port: 3800
+    }
 })
